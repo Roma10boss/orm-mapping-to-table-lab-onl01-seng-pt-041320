93e713f5d2fb9f090052ebd09dae3c9314fa03b2
Romario Gustave <romagustave10@gmail.com>
2020-06-18 18:40:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,12 +29,13 @@ class Student
      DB[:conn].execute(sql)
    end
 
-  def save
-    sql = <<-SQL
-    INSERT INTO students (name,grade)
-    VALUE (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
+   def save
+     sql = <<-SQL
+       INSERT INTO students (name, grade)
+         VALUES (?, ?)
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.grade)
+   end
 
 end
